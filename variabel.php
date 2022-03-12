e8bb8dfd32b08c9438ebf9b08ff1cfdd26c45cc7
alaziz31 <wachidalaziz@gmail.com>
2022-03-13 00:47:06 +0700

    membuat file variabel.php

---
+++ variabel.php
@@ -0,0 +1,14 @@
+<?php
+
+$angka1 = 10;
+$angka2 = 2;
+$tambah = $angka1-$angka2;
+$kursng = $angka1+$angka2;
+$kali = $angka1*$angka2;
+$bagi = $angka1/$angka2;
+
+echo "Angka 1 = 10 dan Angka 2 = 2";
+echo "ditambah {$tambah} <br>";
+echo "dikurangi {$kurang} <br>";
+echo "dikali {$kali} <br>";
+echo "dibagi {$bagi} <br>";
